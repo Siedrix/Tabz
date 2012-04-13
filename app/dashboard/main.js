54634e99c060c108c545a97bfcf39f12ba229d5c
Siedrix <siedrix@gmail.com>
2012-04-13 01:15:20 -0500

    Starting new version of dashboard

--- app/dashboard/main.js
+++ app/dashboard/main.js
@@ -11,61 +11,22 @@ $(document).ready(function(){
 	$.template( "bookmarksTemplate", $('#bookmarksTemplate'));
 
 	$(window).resize(function() {
-		$('nav').jScrollPane();
+		tabz.position();
 	});
 
 	var tabz = {
 		actions : {},
-		user : (new User.Model).sync()
-	};
+		user : (new User.Model).sync(),
+		position : function(){
+			var $container = $('#main');
+			var innerWidth = Math.min( $container.innerWidth(), $container.width() );
+			var units      = Math.floor( innerWidth / 300);
+			var unitWidth  = Math.floor( innerWidth / units );
+			$('.tab').width( unitWidth )
 
-	tabz.actions.requestReadItLater = function(){
-		if( tabz.user.get('hasReadItLater') ){
-			console.log('Render read it later list');
-			chrome.extension.sendRequest({
-				type : 'getReadItLater'
-			},function(response){
-				$('#pages').html('').attr('class','');
-				$('nav').attr('class','');
-				$.each(response.pages, function(i, item){
-					$.tmpl( "pagesTemplate", item ).data('page',item).appendTo( "#pages" );		
-					$('nav').jScrollPane().addClass('readItLater');
-				});
-			});
-		}else{
-			$('#pages').html('').attr('class','');
-			$('nav').attr('class','');				
-			$.tmpl( "logInToReadLaterTemplate", {} ).appendTo( "#pages" );			
+			$container.isotope({itemSelector: '.tab'});
 		}
-	}
-
-	tabz.actions.requestChromeBookmarks = function(){
-		chrome.extension.sendRequest({
-			type : 'getChromeBookmarks'
-		},function(response){
-			$('#pages').html('');
-			$('nav').attr('class','');			
-			$.each(response.bookmarks, function(i, item){
-				$.tmpl( "bookmarksTemplate", item ).data('app',item).appendTo( "#pages" );
-				$('nav').jScrollPane().addClass('chromeBookmarks');
-			});									
-		});
-	}
-
-	tabz.actions.requestChromeApps = function(){	
-		chrome.extension.sendRequest({
-			type : 'getChromeApps'
-		},function(response){
-			//console.log(response);
-			$('#pages').html('');
-			$('nav').attr('class','');
-			$.each(response.apps, function(i, item){			
-				$.tmpl( "appsTemplate", item ).data('app',item).appendTo( "#pages" );		
-				$('nav').jScrollPane().addClass('chromeApps');
-			});						
-		});
-	}	
-
+	};
 	
 	chrome.extension.sendRequest({
 		type : 'getOpenTabs'
@@ -79,27 +40,10 @@ $(document).ready(function(){
 
 		});
 
-		var $container = $('#main');
-		$container.isotope({        
-			itemSelector: '.tab'      
-		});
-				
-		setTimeout(function () {
-			$container.isotope({        
-				itemSelector: '.tab'      
-			});
-		}, 500)
+		tabz.position();
 	});
 
 	window.tabz = tabz;
-
-	var background = localStorage.getItem('background');
-	if(background){
-		backgroundImage = $('<style/>')
-		 .text('body{background-image:url('+background+')}')
-		 .appendTo('head');
-	}
-	delete background;
 
 	window.port = chrome.extension.connect({name: "dashboard"});
 	port.onMessage.addListener(function(msg) {
